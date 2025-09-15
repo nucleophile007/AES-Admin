f0f8d5ae599d720b189d889ae5eb7594bfd6812e
nucleophile007 <dkdps3212@gmail.com>
2025-09-15 13:31:52 +0530

    Fix duplicate PrismaPlugin declaration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,7 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
-
-
-/** @type {import('next').NextConfig} *//** @type {import('next').NextConfig} */
-
-const nextConfig = {const nextConfig = {
-
-  webpack: (config, { isServer }) => {  webpack: (config, { isServer }) => {
-
-    if (isServer) {    if (isServer) {
-
-      config.plugins = [...config.plugins, new PrismaPlugin()];      config.plugins = [...config.plugins, new PrismaPlugin()];
-
-    }    }
-
-    return config;    return config;
-
-  },  },
-
-};};
-
-
-
-module.exports = nextConfig;module.exports = nextConfig;NextConfig } from "next";
-
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
@@ -35,4 +10,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
